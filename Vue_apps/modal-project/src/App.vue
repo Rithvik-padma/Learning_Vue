476fa2fc0ba02bd76bc454136ae3c3693c8d4d97
Rithvik-padma <99183441+Rithvik-padma@users.noreply.github.com>
2023-03-05 01:29:09 +0530

    Started Vue CLI Learning

---
+++ Vue_apps/modal-project/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <h1>{{ title }}</h1>
+  <Modal/>
+</template>
+<script>
+import Modal from './components/Modal.vue'
+
+export default {
+  name: 'App',
+  data(){
+    return{
+      title: "This is my first Vue-app:)"
+    }
+  },
+  methods:{},
+  components: {
+    Modal
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  opacity: 0.8;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ Vue_apps/modal-project/src/components/Modal.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="backdrop">
+        <div class="modal">
+            <p>Okay</p>
+        </div>
+    </div>
+</template>
+<style scoped>
+.modal{
+    width: 400px;
+    padding: 20px;
+    margin: 125px auto;
+    background-color: white;
+    border-radius: 10px;
+}
+.backdrop{
+    top: 0;
+    position: fixed;
+    background-color: rgba(0, 0, 0, 0.55);
+    width: 100%;
+    height: 100%;
+}
+</style>
